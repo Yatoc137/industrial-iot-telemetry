eaa407d2bed7ce44934ae99185a415518d2b2343
Henrique Rodrigues Santos <hiqkrs@gmail.com>
2025-11-28 17:49:43 -0300

    docs(firmware): adiciona comentarios no utils.h

--- firmware/include/utils.h
+++ firmware/include/utils.h
@@ -4,19 +4,22 @@
 extern "C" {
 #endif
 
+// temperature Units
 typedef enum {
-    FAHRENHEIT,
-    CELSIUS,
-    KELVIN
+    FAHRENHEIT, // Fahrenheit
+    CELSIUS,    // Celsius
+    KELVIN      // kelvin
 } UTIL_TEMP;
 
+// pressure units
 typedef enum {
-    BAR,
-    PSI,
-    KGFCM2,
-    MMH2O
+    BAR,     // Bar
+    PSI,     // PSI
+    KGFCM2,  // KgF/CM²
+    MMH2O    // mmH2O
 } UTIL_PRESSURE;
 
+// mapping function
 static inline float util_map(float value, float in_min, float in_max, float out_min, float out_max) {
     if (value <= in_min) return out_min;
     if (value >= in_max) return out_max;
@@ -24,20 +27,24 @@ static inline float util_map(float value, float in_min, float in_max, float out_
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+// smoothing function
 static inline float util_smooth(float new_value, float *state) {
-    *state = (*state) * 0.9f + new_value * 0.1f;
+    *state = (*state) * 0.9f + new_value * 0.1f; // 90% from old value and 10% from the new value
     return *state;
 }
 
+// temperature unit conversion function 
 static inline float util_tempConvert(UTIL_TEMP from, float v, UTIL_TEMP to) {
     float k = 0.0f;
 
+    // converts value to kelvin
     switch (from) {
         case CELSIUS:     k = v + 273.15f; break;
         case FAHRENHEIT:  k = (v - 32.0f) * 5.0f/9.0f + 273.15f; break;
         case KELVIN:      k = v; break;
     }
 
+    // converts value (in kelvin) to requested unit
     switch (to) {
         case CELSIUS:     return k - 273.15f;
         case FAHRENHEIT:  return (k - 273.15f) * 9.0f/5.0f + 32.0f;
@@ -47,9 +54,11 @@ static inline float util_tempConvert(UTIL_TEMP from, float v, UTIL_TEMP to) {
     return v;
 }
 
+// pressure conversion function
 static inline float util_pressureConvert(UTIL_PRESSURE from, float v, UTIL_PRESSURE to) {
     float kpa = 0.0f;
 
+    // converts value to kPa
     switch (from) {
         case BAR:     kpa = v * 100.0f;        break;
         case PSI:     kpa = v * 6.894757f;     break;
@@ -57,6 +66,7 @@ static inline float util_pressureConvert(UTIL_PRESSURE from, float v, UTIL_PRESS
         case MMH2O:   kpa = v * 0.00980665f;   break;
     }
 
+    // converts value (in kPa) to requested unit
     switch (to) {
         case BAR:     return kpa * 0.01f;
         case PSI:     return kpa * 0.1450377f;
